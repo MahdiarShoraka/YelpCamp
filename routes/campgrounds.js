08ddab2703d4a5b5c5098c4baf33acf56a3f3434
Mahdiar Shoraka <mahdiarshoraka@gmail.com>
2024-12-26 19:03:10 -0500

    Implemented Express router to organize the files better for scaling

---
+++ routes/campgrounds.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const catchAsync = require("../utilities/catchAsync");
+const ExpressError = require("../utilities/ExpressError");
+const { campgroundSchema } = require("../schemas");
+const Campground = require("../models/campground");
+const router = express.Router({ mergeParams: true });
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(message, 400);
+  } else {
+    // if data is valid, call the next middleware function (e.g., catchAsync in post)
+    next();
+  }
+};
+
+router.get(
+  "/",
+  catchAsync(async function (req, res, next) {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+router.get("/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
+    res.render("campgrounds/show", { campground: campground });
+  })
+);
+
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res, next) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+router.put(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;
---
+++ routes/reviews.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const catchAsync = require("../utilities/catchAsync");
+const ExpressError = require("../utilities/ExpressError");
+const { reviewSchema } = require("../schemas");
+const Review = require("../models/review");
+const Campground = require("../models/campground");
+const router = express.Router({ mergeParams: true });
+
+const validateReview = (req, res, next) => {
+  const { error } = reviewSchema.validate(req.body);
+  if (error) {
+    const message = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(message, 400);
+  } else {
+    // if data is valid, call the next middleware function (e.g., catchAsync in post)
+    next();
+  }
+};
+
+router.post(
+  "/",
+  validateReview,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+router.delete(
+  "/:reviewId",
+  catchAsync(async (req, res) => {
+    const { reviewId, id } = req.params;
+    await Campground.findByIdAndUpdate(id, {
+      $pull: { reviews: reviewId },
+    });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+  })
+);
+
+module.exports = router;
