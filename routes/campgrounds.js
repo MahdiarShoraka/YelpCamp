08a0e13e0c8a52e1fc240d79262874d668a7d47b
Mahdiar Shoraka <mahdiarshoraka@gmail.com>
2024-12-31 19:35:06 -0500

    Added cloudinary and image upload. not complete yet

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,17 +2,23 @@ const express = require("express");
 const catchAsync = require("../utilities/catchAsync");
 const { validateCampground, isAuthor, isLoggedIn } = require("../middleware");
 const campgrounds = require("../controllers/campgrounds");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
 const router = express.Router();
 
 router
   .route("/")
   .get(catchAsync(campgrounds.index))
-  .post(
-    isLoggedIn,
-    validateCampground,
-    catchAsync(campgrounds.createCampground)
-  );
+  .post(upload.array("image"), (req, res) => {
+    console.log(req.body, req.files);
+  });
+// .post(
+//   isLoggedIn,
+//   validateCampground,
+//   catchAsync(campgrounds.createCampground)
+// );
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 // order matters (new before :id)
