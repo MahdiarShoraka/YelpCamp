fbe1d492d6949412f693bfb19a8c7ebd18c2daa2
Mahdiar Shoraka <mahdiarshoraka@gmail.com>
2025-01-04 13:13:36 -0500

    Added the dynamic clustered map to the index page. improved the style.

---
+++ public/javascripts/clusterMap.js
@@ -0,0 +1,104 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/light-v11",
+  center: [-103.5917, 40.6699],
+  zoom: 3,
+});
+
+map.on("load", () => {
+  map.addSource("campgrounds", {
+    type: "geojson",
+    // received from index.ejs
+    data: campgrounds,
+    cluster: true,
+    clusterMaxZoom: 14, // Max zoom to cluster points on
+    clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+  });
+
+  map.addLayer({
+    id: "clusters",
+    type: "circle",
+    source: "campgrounds",
+    filter: ["has", "point_count"],
+    paint: {
+      "circle-color": [
+        "step",
+        ["get", "point_count"],
+        "#51bbd6",
+        30,
+        "#f1f075",
+        80,
+        "#f28cb1",
+      ],
+      "circle-radius": ["step", ["get", "point_count"], 15, 30, 25, 80, 35],
+    },
+  });
+
+  map.addLayer({
+    id: "cluster-count",
+    type: "symbol",
+    source: "campgrounds",
+    filter: ["has", "point_count"],
+    layout: {
+      "text-field": ["get", "point_count_abbreviated"],
+      "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+      "text-size": 12,
+    },
+  });
+
+  map.addLayer({
+    id: "unclustered-point",
+    type: "circle",
+    source: "campgrounds",
+    filter: ["!", ["has", "point_count"]],
+    paint: {
+      "circle-color": "#11b4da",
+      "circle-radius": 5,
+      "circle-stroke-width": 1,
+      "circle-stroke-color": "#faa",
+    },
+  });
+
+  // inspect a cluster on click
+  map.on("click", "clusters", (e) => {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: ["clusters"],
+    });
+    const clusterId = features[0].properties.cluster_id;
+    map
+      .getSource("campgrounds")
+      .getClusterExpansionZoom(clusterId, (err, zoom) => {
+        if (err) return;
+
+        map.easeTo({
+          center: features[0].geometry.coordinates,
+          zoom: zoom,
+        });
+      });
+  });
+
+  // just a single point (campground)
+  map.on("click", "unclustered-point", (e) => {
+    const { popupMarkup } = e.features[0].properties;
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    // Ensure that if the map is zoomed out such that
+    // multiple copies of the feature are visible, the
+    // popup appears over the copy being pointed to.
+    if (["mercator", "equirectangular"].includes(map.getProjection().name)) {
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+    }
+
+    new mapboxgl.Popup().setLngLat(coordinates).setHTML(popupMarkup).addTo(map);
+  });
+
+  map.on("mouseenter", "clusters", () => {
+    map.getCanvas().style.cursor = "pointer";
+  });
+  map.on("mouseleave", "clusters", () => {
+    map.getCanvas().style.cursor = "";
+  });
+});
--- seeds/index.js
+++ seeds/index.js
@@ -15,50 +15,26 @@ const pickSample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 30) + 10;
+    // Default values for a new campground
     const camp = new Campground({
       author: "67731b999add29adf59234be",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${pickSample(descriptors)} ${pickSample(places)}`,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
-          url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838370/YelpCamp/awrowc4m2wjji4h0vkdv.jpg",
-          filename: "YelpCamp/awrowc4m2wjji4h0vkdv",
+          url: "https://res.cloudinary.com/dptgg37uu/image/upload/w_800,ar_1:1,c_fill,g_auto,e_art:hokusai/v1735942688/YelpCamp/krvrt7hbruwmjl1cxwyn.jpg",
+          filename: "YelpCamp/krvrt7hbruwmjl1cxwyn",
         },
-        {
-          url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838371/YelpCamp/otn86mthv6ltyfio6upu.jpg",
-          filename: "YelpCamp/otn86mthv6ltyfio6upu",
-        },
-        // {
-        //   url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838371/YelpCamp/vpzfsdjie9cj14hbxc4m.jpg",
-        //   filename: "YelpCamp/vpzfsdjie9cj14hbxc4m",
-        // },
-        // {
-        //   url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838372/YelpCamp/bitu9mlvpppugkdhd6se.jpg",
-        //   filename: "YelpCamp/bitu9mlvpppugkdhd6se",
-        // },
-        // {
-        //   url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838372/YelpCamp/ug04si7jnygup4msq2qg.jpg",
-        //   filename: "YelpCamp/ug04si7jnygup4msq2qg",
-        // },
-        // {
-        //   url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838372/YelpCamp/lyrd7iyjetjmb9jhdgxn.jpg",
-        //   filename: "YelpCamp/lyrd7iyjetjmb9jhdgxn",
-        // },
-        // {
-        //   url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838372/YelpCamp/dbmj4ttubes2c9wd67lc.jpg",
-        //   filename: "YelpCamp/dbmj4ttubes2c9wd67lc",
-        // },
-        // {
-        //   url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838372/YelpCamp/xvy0wbrs4qzcwsllzk2i.jpg",
-        //   filename: "YelpCamp/xvy0wbrs4qzcwsllzk2i",
-        // },
-        // {
-        //   url: "https://res.cloudinary.com/dptgg37uu/image/upload/v1735838373/YelpCamp/c1qyptyhxlw3fexwpm2a.jpg",
-        //   filename: "YelpCamp/c1qyptyhxlw3fexwpm2a",
-        // },
       ],
       description:
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, quo obcaecati iure itaque voluptatum doloribus blanditiis exercitationem ducimus numquam. Praesentium assumenda dignissimos facilis dolorum officia aperiam minima voluptatum vero unde.",
