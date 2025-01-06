4ff3a9d20888ac8018ff4438f76e96ccc93a2c60
Mahdiar Shoraka <mahdiarshoraka@gmail.com>
2025-01-06 12:47:55 -0500

    minor fixes to app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const methodOverride = require("method-override");
 const ExpressError = require("./utilities/ExpressError");
@@ -15,29 +14,13 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const mongoSanitize = require("express-mongo-sanitize");
-
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews.js");
 const usersRoutes = require("./routes/users.js");
-
-const app = express();
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(mongoSanitize());
+const MongoStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 const secret = process.env.SECRET || "thisisabackupsecret";
-
-// Passport configuration
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 mongoose.connect(dbUrl, {});
 const db = mongoose.connection;
@@ -46,12 +29,25 @@ db.once("open", () => {
   console.log("database connected");
 });
 
+const app = express();
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(mongoSanitize({ replaceWith: "_" }));
+
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
   crypto: {
     secret,
   },
+});
+
+store.on("error", function (e) {
+  console.log("session store error", e);
 });
 
 const sessionConfig = {
@@ -70,11 +66,14 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
-store.on("error", function (e) {
-  console.log("session store error", e);
-});
+// Passport configuration
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-// Flash accessible by all views
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -83,9 +82,9 @@ app.use((req, res, next) => {
 });
 
 // Prefix and use Express Routers
+app.use("/", usersRoutes);
 app.use("/campgrounds", campgroundsRoutes);
 app.use("/campgrounds/:id/reviews", reviewsRoutes);
-app.use("/", usersRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
